fdadb18e069f9da3cfbb50f16f3167c721c79c02
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-03-20 17:56:01 +0700

    Update Function upload image: delete image in temp folder

--- Server/src/services/medias.services.ts
+++ Server/src/services/medias.services.ts
@@ -1,26 +1,22 @@
 import { Request } from 'express'
 import { getNameFromFullname, handleUploadImage } from '~/utils/file'
 import sharp from 'sharp'
-import { UPLOAD_IMAGE_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_DIR, UPLOAD_IMAGE_TEMP_DIR } from '~/constants/dir'
 import path from 'path'
-import fs from 'fs'
 import { envConfig, isProduction } from '~/constants/config'
 import { MediaType } from '~/constants/enums'
 import { Media } from '~/models/Other'
+import { exec } from 'child_process'
 
 class MediasService {
   async handleUploadImage(req: Request) {
+    const directoryPathImageTemp = path.resolve(__dirname, UPLOAD_IMAGE_TEMP_DIR)
     const files = await handleUploadImage(req)
     const result: Media[] = await Promise.all(
       files.map(async (file) => {
         const newName = getNameFromFullname(file.newFilename)
         const newPath = path.resolve(UPLOAD_IMAGE_DIR, `${newName}.jpg`)
         await sharp(file.filepath).jpeg().toFile(newPath)
-        try {
-          fs.unlinkSync(file.filepath)
-        } catch (error) {
-          console.log(error)
-        }
         return {
           url: isProduction
             ? `${envConfig.host}/static/image/${newName}.jpg`
@@ -29,6 +25,37 @@ class MediasService {
         }
       })
     )
+
+    if (process.platform === 'win32') {
+      exec(`del /Q ${directoryPathImageTemp}\\*`, (error, stdout, stderr) => {
+        if (error) {
+          console.error('Error occurred while deleting files:', error)
+          return
+        }
+
+        if (stderr) {
+          console.error('Error output:', stderr)
+          return
+        }
+
+        return stdout
+      })
+    } else {
+      exec(`rm -f ${directoryPathImageTemp}/*`, (error, stdout, stderr) => {
+        if (error) {
+          console.error('Error occurred while deleting files:', error)
+          return
+        }
+
+        if (stderr) {
+          console.error('Error output:', stderr)
+          return
+        }
+
+        return stdout
+      })
+    }
+
     return result
   }
 }
