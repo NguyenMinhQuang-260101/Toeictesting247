978f8c1b7dca8cd3bf63dcedbf7daec267cfd3e3
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-03-31 15:10:18 +0700

    Fix bug about key AWS

--- Server/src/middlewares/error.middlewares.ts
+++ Server/src/middlewares/error.middlewares.ts
@@ -3,17 +3,29 @@ import { omit } from 'lodash'
 import HTTP_STATUS from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/Errors'
 
-export const defaultErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof ErrorWithStatus) {
-    return res.status(error.status).json(omit(error, ['status']))
+export const defaultErrorHandle = (err: any, req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status).json(omit(err, ['status']))
+    }
+    const finalError: any = {}
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
+    })
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: finalError.message,
+      errorInfo: omit(finalError, ['stack'])
+    })
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal Server Error',
+      errorInfo: omit(error as any, ['stack'])
+    })
   }
-
-  Object.getOwnPropertyNames(error).forEach((key) => {
-    Object.defineProperty(error, key, { enumerable: true })
-  })
-
-  res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: error.message,
-    errorInfo: omit(error, ['stack'])
-  })
 }
