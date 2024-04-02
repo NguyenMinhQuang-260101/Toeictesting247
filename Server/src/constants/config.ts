34d23178bb1a552a10ac5508caa5c9189b376f1d
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-04-02 12:31:23 +0700

    Update config and add .env.example

--- Server/src/constants/config.ts
+++ Server/src/constants/config.ts
@@ -1,10 +1,12 @@
 import { config } from 'dotenv'
 import argv from 'minimist'
-config()
 
 const options = argv(process.argv.slice(2))
-
-export const isProduction = Boolean(options.production)
+console.log(options)
+export const isProduction = options.env === 'production'
+config({
+  path: options.env ? `.env.${options.env}` : '.env'
+})
 
 export const envConfig = {
   port: (process.env.PORT as string) || 4000,
