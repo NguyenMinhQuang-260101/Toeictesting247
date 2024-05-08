d1bfe69aa5d751dabf62fd6c950b6974988cb354
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-05-08 19:28:14 +0700

    Increase request limit to 1000 in 10 min

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -112,8 +112,8 @@ app.use(helmet())
 
 // ** Rate limiting **
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  limit: 1000, // Limit each IP to 1000 requests per `window` (here, per 15 minutes).
   standardHeaders: true, // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false // Disable the `X-RateLimit-*` headers.
   // store: ... , // Use an external store for consistency across multiple server instances.
